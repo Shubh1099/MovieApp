e52139546cc3b15b67241a6a8db20463b1001920
Shubh1099 <shubhammane434@gmail.com>
2025-02-08 21:55:55 +0530

    first_commit

---
+++ MyMovies/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import UploadArea from "./components/UploadArea";
+import ViewArea from "./components/ViewArea";
+function App() {
+  return (
+    <>
+      <NavBar  />
+      <UploadArea/>
+      <ViewArea/>
+    </>
+  );
+}
+
+export default App;
---
+++ MyMovies/src/components/ViewArea.jsx
@@ -0,0 +1,13 @@
+function ViewArea(){
+    return (
+      <>
+        <div className="bg-gray-400 grid-rows-3">
+          <div className="">zcvxgsdafasf</div>
+          <video width="1920" height="1080" src="https://www.youtube.com/watch?v=ric-95ig5oE"></video>
+          <div className="text-4xl">gwsgrewsgerwg</div>
+        </div>
+      </>
+    );
+}
+
+export default ViewArea;
